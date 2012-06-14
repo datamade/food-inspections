6cbfbb4027f27a826aefc114fb0e8a766246998a
Forest Gregg <fgregg@uchicago.edu>
2012-06-14 16:03:45 -0500

    first draft for lohff

--- inspections.R
+++ inspections.R
@@ -1,4 +1,5 @@
 library(MASS)
+library(RSQLite)
 
 #bakery, banquet hall, candy store, caterer, coffee shop, day care center (for ages less than 2), day care center (for ages 2 ¡V 6), day care center (combo, for ages less than 2 and 2 ¡V 6 combined), gas station, Golden Diner, grocery store, hospital, long term care center(nursing home), liquor store, mobile food dispenser, restaurant, paleteria, school, shelter, tavern, social club, wholesaler, or Wrigley Field Rooftop.
 
@@ -12,77 +13,65 @@ agg.test <- function(x, threshold) {
   return(names(x.table)[x.table > threshold])
 }
 
-
-# Make sure that a license is associated with one and only one
-# facility type
-testData <- function(inspections) {
-  licenses <- unique(inspections$License..)
-  for (license in licenses) {
-    license.inspections = inspections[inspections$License.. == license, ]
-    if (all(license.inspections$Facility.Type == license.inspections$Facility.Type[1]) == FALSE) { 
-      print(license.inspections[, c("License..", "Address", "DBA.Name", "AKA.Name", "Facility.Type")])
-    }
-  }
-}
-
 interVisitTimes <- function(inspections, facility.type) {
   ivt = c()
   #subset inspections
-  inspections = inspections[inspections$Facility.Type == facility.type,
-    c("License..", "date.num", "Inspection.Type", "Results")]
+  inspections = inspections[inspections$facility_type == facility.type,
+    c("license_no", "inspection_date", "inspection_type", "results")]
 
   print(dim(inspections))
 
-  licenses <- unique(inspections$License..)
+  licenses <- unique(inspections$license_no)
   for (license in licenses) {
-    visits <- inspections[inspections$License.. == license,]
-    visits <- visits[order(visits$date.num),]    
+    visits <- inspections[inspections$license_no == license,]
+    visits <- visits[order(visits$inspection_date),]    
 
     visits.num = dim(visits)[1]
     if (visits.num > 2) {
-      print(visits)
       for (i in 1:(visits.num-1)) {
-        if (visits[i, "Inspection.Type"] == "Canvass" &
-            visits[i, "Results"] == 1 &
-            visits[i+1, "Inspection.Type"] == "Canvass") {
-          inter.visit.time <- (visits[i+1, "date.num"]
-                               - visits[i, "date.num"])
-          if (inter.visit.time > 600) {
-            print(visits)
-          }
-          ivt = c(ivt, visits[i+1, "date.num"] - visits[i, "date.num"])
-        }
+        inter.visit.time <- (visits[i+1, "inspection_date"]
+                             - visits[i, "inspection_date"])
+        #if (inter.visit.time > 600) {
+        #  print(visits)
+        #}
+        #if (visits[i, "results"] == "Out of Business" & i != visits.num) {
+        #  print(visits)
+        #}
+        ivt = c(ivt, inter.visit.time)
       }
     }
   }
   return(ivt)
 }
 
-inspections <- read.csv("./ChicagoFoodInspections.csv")
+drv <- dbDriver("SQLite")
+con <- dbConnect(drv, "./inspections.db")
+
+inspections<- dbGetQuery(con, "SELECT * FROM inspections_clean")
+
+last.inspections <- dbGetQuery(con, "SELECT *
+    FROM inspections_clean INNER JOIN (SELECT license_no AS ln, MAX(inspection_date) AS last_inspection FROM inspections_clean GROUP BY license_no) ON license_no=ln AND inspection_date = last_inspection WHERE results != 'Out of Business' and facility_type = 'restaurant'")
+
+
+
+dbDisconnect(con)
 
 print(dim(inspections))
 
-inspections <- na.omit(inspections)
-  
-inspections$Inspection.Date <- as.Date(inspections$Inspection.Date)
-inspections$date.num <- as.numeric(inspections$Inspection.Date)
+inspections$inspection_date <- as.Date(inspections$inspection_date)
+last.inspections$inspection_date <- as.Date(last.inspections$inspection_date)
 
-inspections$Facility.Type <- factor(tolower(inspections$Facility.Type))
 
-inspections$Facility.Type[inspections$Facility.Type == "wrigley roof top"] <- "wrigley rooftop"
-inspections$Facility.Type[inspections$Facility.Type == "wrigley roof top"] <- "wrigley rooftop"
+ivt = interVisitTimes(inspections, "restaurant")
 
-common.facility.types <- agg.test(inspections$Facility.Type, 100)
 
-inspections$Risk <- as.ordered(inspections$Risk)
-inspections$Facility.Type <- factor(inspections$Facility.Type)
-inspections$Facility.Type <- relevel(inspections$Facility.Type,
-                                     ref="restaurant"
-                                     )
+br = seq(0, 949, 73)
+hist(ivt, breaks=br, xaxt="n", ylim=c(0,2000))
+axis(1, at=365)
 
-inspections$Results <- as.ordered(inspections$Results)
-
-#testData(inspections)
-#ivt = interVisitTimes(inspections, "restaurant")
-  
-             
+hist(as.numeric(Sys.Date() - last.inspections$inspection_date),
+     breaks=br,
+     xaxt="n",
+     ylim=c(0,2000))
+     )
+axis(1, at=365)
--- updateInspections.sh
+++ updateInspections.sh
@@ -1,1 +1,2 @@
 wget --output-document ChicagoFoodInspections.csv https://data.cityofchicago.org/api/views/4ijn-s7e5/rows.csv?accessType=DOWNLOAD 
+#wget --output-document LastInspections.csv http://www.google.com/fusiontables/api/query?sql=SELECT+*+FROM+3443286
